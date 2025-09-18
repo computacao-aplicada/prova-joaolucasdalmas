f8f030c9632b6d4d18b27b84499aecc2dbbe6a26
joaolucasdalmas <joaolucas.dalmas@gmail.com>
2025-09-17 21:51:42 -0300

    Testes finalizados!!!!!!!!!!!!1

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -9,4 +9,21 @@ public class ValidadorTest {
         assertTrue(Validador.validarCPF("529.982.247-25"));
         assertTrue(Validador.validarCPF("52998224725"));
     }
+    @Test
+    void deveRejeitarEntradasInvalidas() {
+        assertFalse(Validador.validarCPF(null));
+        assertFalse(Validador.validarCPF(""));
+        assertFalse(Validador.validarCPF("529.982.247-2X"));
+        assertFalse(Validador.validarCPF("00000000000"));
+    }
+    @Test
+    void deveRejeitarTamanhosIncorretos() {
+        assertFalse(Validador.validarCPF("935.411.347-8"));   // 10 dígitos
+        assertFalse(Validador.validarCPF("935.411.347-800")); // 12 dígitos
+    }
+    @Test
+    void deveRejeitarDVIncorreto() {
+        assertFalse(Validador.validarCPF("529.982.247-24"));
+        assertFalse(Validador.validarCPF("123.456.789-00"));
+    }
 }
